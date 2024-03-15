54520c659a7a5e72e05f7df8c4aa7312cc4adb60
dkool <dkool@dkools-MacBook-Pro.local>
2024-03-15 15:15:30 +0100

    feat: update documentation

--- examples/default/locals.tf
+++ examples/default/locals.tf
@@ -13,7 +13,7 @@ locals {
       name                           = "pe-demo-dev-blob"
       subnet_id                      = module.network.subnets.sn1.id
       private_connection_resource_id = module.storage.account.id
-      private_dns_zone_ids           = [module.private_dns.zones.vault.id]
+      private_dns_zone_ids           = [module.private_dns.zones.blob.id]
       subresource_names              = ["blob"]
     }
     table = {
@@ -22,6 +22,27 @@ locals {
       private_connection_resource_id = module.storage.account.id
       private_dns_zone_ids           = [module.private_dns.zones.table.id]
       subresource_names              = ["table"]
+    }
+  }
+}
+
+locals {
+  zones = {
+    blob = {
+      name = "privatelink.blob.core.windows.net"
+      virtual_network_links = {
+        link1 = {
+          virtual_network_id = module.network.vnet.id
+        }
+      }
+    }
+    table = {
+      name = "privatelink.table.core.windows.net"
+      virtual_network_links = {
+        link1 = {
+          virtual_network_id = module.network.vnet.id
+        }
+      }
     }
   }
 }
