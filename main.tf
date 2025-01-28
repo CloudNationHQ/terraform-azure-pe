54f2d29b0e57e82603937405f1c259f84ac1e5bb
Jasper Post <jasper.post@cloudnation.nl>
2025-01-28 21:38:03 +0100

    fix: This is an optional value

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_private_endpoint" "endpoint" {
   private_service_connection {
     name                              = each.key
     is_manual_connection              = try(each.value.is_manual_connection, false)
-    private_connection_resource_id    = each.value.private_connection_resource_id
+    private_connection_resource_id    = try(each.value.private_connection_resource_id, null)
     subresource_names                 = each.value.subresource_names
     private_connection_resource_alias = try(each.value.private_connection_resource_alias, null)
     request_message                   = try(each.value.request_message, null)
